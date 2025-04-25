e2705585d3c385c19e72d99f71e143a3a96c04a8
akasht29 <akasht9000@gmail.com>
2025-04-25 01:29:29 -0700

    done creates topics

---
+++ kafka/create-topics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BROKER="localhost:9092"
+TOPICS=("twitter_sentiment" "reddit_stream" "sports_stream")
+
+
+# Proceed to create topics
+for topic in "${TOPICS[@]}"; do
+  echo "🔧 Creating topic: $topic"
+  kafka-topics --create \
+    --if-not-exists \
+    --bootstrap-server "$BROKER" \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic "$topic"
+done
